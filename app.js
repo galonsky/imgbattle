3991b6b7b3b8837c79f41e579d3200fe48da3060
Alex Galonsky <alex@galonsky.com>
2011-03-27 23:48:04 -0400

    Base server and layout

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express.createServer();
+
+app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+	app.use(express.logger());
+	app.use(express.static(__dirname + '/static'));
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.listen(4000);
